a6b77dd0f3bb9d9de7250a9d4a7482414e603424
rrchandler <ryanchandler50@gmail.com>
2020-12-02 22:27:23 -0800

    Added a test for iterators

---
+++ test_iterator.hpp
@@ -0,0 +1,24 @@
+#ifndef __TEST_ITERATOR_HPP__
+#define __TEST_ITERATOR_HPP__
+
+#include "gtest/gtest.h"
+#include "add.hpp"
+#include "preorder_iterator.cpp"
+#include "iterator.hpp"
+#include "sub.hpp"
+#include "op.hpp"
+#include <vector>
+TEST(PreOrderTest, Regular) {
+	vector<Base*> vec;
+	Op* three = new Op(3);
+	Op* two = new Op(2)
+	Add* add1 = new Add(three, two);
+	Iterator* it = new Iterator(add1);
+	it->first();
+	while(!it->is_done()) {
+		vec.push_back(it->current());
+	}
+	ASSERT_EQ(vec.size(), 2);
+	EXPECT_EQ(vec.at(0)->evaluate() == 3);
+	EXPECT_EQ(vec.at(1)->evaluate() == 2);	
+}
